903bbaddf404f35cf47a96880d8ef650f378a889
luna-nightbyte <lunawifu18@gmail.com>
2024-12-21 17:21:13 +0100

    removed version setting in run()

--- internal/core.py
+++ internal/core.py
@@ -6,6 +6,7 @@ import cv2
 
 
 import internal.detection as detection
+import internal.models.face_detector as faceDet
 
 AI = None
 # Initialize Mediapipe face detection
@@ -25,17 +26,11 @@ def run():
     output_dir = args.out
     csv_file = os.path.join(output_dir, args.csv)
     output_size = (args.size,args.size)
+    conf = args.confidence
     
-    import internal.models.face_detector as faceDet
-
     AI = faceDet.Face_Detector().init(args.version, args.model_selection)
-    AI.setVersion(args.version)
-    if AI.version == 2:
-        modelName = f"{args.model_selection}"
-    else:
-        modelName = args.model_selection
-        
-    AI.load(modelName, args.confidence)
+    
+    AI.load(AI.name(), conf)
     if not AI.up():
         print("Error loading model")
         return
